8412bbc51d1d25624b290cbab9d79e733866a178
Pasha <pashaalbara01@gmail.com>
2024-10-07 21:07:17 +0700

    cleared: forgot password

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -80,7 +80,6 @@
                                           <input type="checkbox" class="form-check-input" id="customCheck1">
                                           <label class="form-check-label" for="customCheck1">Remember Me</label>
                                       </div>
-                                      <a href="recoverpw.html">Forgot Password?</a>
                                   </div>
                               </div>
                               <div class="d-flex justify-content-center">
